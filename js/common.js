b6b4f718cf01b8dc90495e746959ba3fa1079ab1
Alexey <Solovey_alexey3@mail.ru>
2016-07-25 09:21:26 +0300

    Start

---
+++ js/common.js
@@ -0,0 +1,33 @@
+;(function($){
+	$(document).ready(function() {
+	        // Declare variables
+	        var width = 400;
+	        var slides = $('#list-images li');
+	        var numSlides = slides.length;
+
+	        // Wrap the slides & set the wrap width - this will be used to animate the slider left/right
+	        slides.wrapAll('<div id="slide-wrap"></div>').css({ 'float': 'left', 'width': width });
+	        $('#slide-wrap').css({ width: width * numSlides });
+
+	        // Hover function - animate the slides based on index of links
+	        $('#list-links li a').hover(function () {
+	            $('#list-links li').removeClass('hover');
+	            var i = $(this).index('#list-links li a');
+	            $(this).parent().addClass('hover');
+	            $('#slide-wrap').stop().animate({ 'marginLeft': width * (-i) });
+	        });
+
+	        $("#owl-example").owlCarousel({
+
+	             items : 2, 
+	             itemsDesktop : false,
+	             itemsDesktopSmall : false,
+	             itemsTablet: false,
+	             itemsMobile : false
+
+	        });
+	});
+
+
+
+})(jQuery)
